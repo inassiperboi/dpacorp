00ff00643ef191deda6d177bf877641ba32754f9
inassiperboi <inas.najah.zhahirah-2022@vokasi.unair.ac.id>
2026-09-13 16:19:28 +0700

    push yeyy

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -7,7 +7,7 @@
 
     <title>@yield('title', 'Admin Panel') — DPA Corp</title>
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="public/images/hero/logo.png">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
     <!-- SweetAlert2 -->
